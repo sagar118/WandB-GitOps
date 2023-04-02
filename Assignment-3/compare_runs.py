7142d43fce09318f1bce1db637e9b07634ec5543
Sagar Thacker <sgrhacker18@gmail.com>
2023-04-02 15:53:21 -0400

    Update compare_runs.py

--- Assignment-3/compare_runs.py
+++ Assignment-3/compare_runs.py
@@ -24,35 +24,37 @@ def comapre_runs(entity='sagar118',
     tag = os.getenv('BASELINE_TAG', tag)
     run_id = os.getenv('RUN_ID', run_id)[0]
 
+    print(run_id)
     assert run_id, 'Run id must be present in the enironment variable or passed as an arguement'
 
-    baseline = get_baseline_scores(entity, project, tag)
-    report = wr.Report(project=project, title="Compare Baseline vs Latest Run")
+#     baseline = get_baseline_scores(entity, project, tag)
+#     report = wr.Report(project=project, title="Compare Baseline vs Latest Run")
     
-    panel_grids = wr.PanelGrid(
-        runsets=[
-            wr.Runset(entity, project, "Run Comparison").set_filters_with_python_expr(f"ID in ['{run_id}', '{baseline.id}']")
-        ],
-        panels = [
-            wr.RunComparer(diff_only='split', layout={'w': 24, 'h': 15})
-        ]
-    )
+#     panel_grids = wr.PanelGrid(
+#         runsets=[
+#             wr.Runset(entity, project, "Run Comparison").set_filters_with_python_expr(f"ID in ['{run_id}', '{baseline.id}']")
+#         ],
+#         panels = [
+#             wr.RunComparer(diff_only='split', layout={'w': 24, 'h': 15})
+#         ]
+#     )
 
-    plots = wr.PanelGrid(
-        panels=[
-            wr.LinePlot(x='Step', y=['acc'], smoothing_factor=0.8),
-            wr.LinePlot(x='Step', y=['loss'], smoothing_factor=0.8),
-            wr.LinePlot(x='Step', y=['val_acc'], smoothing_factor=0.8),
-            wr.LinePlot(x='Step', y=['val_loss'], smoothing_factor=0.8),
-        ]
-    )
-    report.blocks = report.blocks[:1] + [panel_grids] + [plots] + report.blocks[1:]
-    report.save()
+#     plots = wr.PanelGrid(
+#         panels=[
+#             wr.LinePlot(x='Step', y=['acc'], smoothing_factor=0.8),
+#             wr.LinePlot(x='Step', y=['loss'], smoothing_factor=0.8),
+#             wr.LinePlot(x='Step', y=['val_acc'], smoothing_factor=0.8),
+#             wr.LinePlot(x='Step', y=['val_loss'], smoothing_factor=0.8),
+#         ]
+#     )
+#     report.blocks = report.blocks[:1] + [panel_grids] + [plots] + report.blocks[1:]
+#     report.save()
 
-    if os.getenv("CI"):
-        with open(os.environ['GITHUB_OUTPUT'], 'a') as handle:
-            print(f"REPORT_URL={report.url}", file=handle)
-    return report.url
+#     if os.getenv("CI"):
+#         with open(os.environ['GITHUB_OUTPUT'], 'a') as handle:
+#             print(f"REPORT_URL={report.url}", file=handle)
+#     return report.url
+      return ""
     
 if __name__ == '__main__':
     print(f"The comparison report can be found at: {comapre_runs()}")
